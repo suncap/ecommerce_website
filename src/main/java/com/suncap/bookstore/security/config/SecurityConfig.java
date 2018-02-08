f65521d4e672a61da9c2857dcc9f0b05023298a3
Unknown <suncap11@gmail.com>
2018-02-08 22:28:11 +0200

    Spring session security - Redis

--- src/main/java/com/suncap/bookstore/security/config/SecurityConfig.java
+++ src/main/java/com/suncap/bookstore/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.suncap.bookstore.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
+
+import com.suncap.bookstore.service.UserSercurityService;
 
 @Configuration
 @EnableWebSecurity
@@ -39,5 +44,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	public HttpSessionStrategy httpSessionStrategy() {
+		return new HeaderHttpSessionStrategy();
 	}
 }
