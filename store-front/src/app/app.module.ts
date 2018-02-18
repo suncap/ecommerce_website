5086b0b02b054ca237e2f4952cf6156f4ca3a2df
Unknown <suncap11@gmail.com>
2018-02-18 14:42:56 +0200

    Router and home component

--- store-front/src/app/app.module.ts
+++ store-front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from './material.module';
+import { routing } from './app.routing';
 
 import 'hammer.js';
 
