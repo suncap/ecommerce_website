0f1e49e149d6a679258674ff2f2dc1f570445263
Unknown <suncap11@gmail.com>
2018-02-18 23:36:33 +0200

    Nav-bar and css

--- store-front/src/app/app.module.ts
+++ store-front/src/app/app.module.ts
@@ -9,11 +9,13 @@ import 'hammer.js';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
