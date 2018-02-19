22271e16cfe5c44fe99d03c2bd5daf77bbe9d55d
Unknown <suncap11@gmail.com>
2018-02-19 23:52:55 +0200

    Account template

--- store-front/src/app/app.routing.ts
+++ store-front/src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
 
 const appRoutes: Routes = [
   {
@@ -11,6 +12,10 @@ const appRoutes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'myAccount',
+    component: MyAccountComponent
   }
 ];
 
