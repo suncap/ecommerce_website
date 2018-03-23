321963101efda88267b301d128ea52c84cf146da
Unknown <suncap11@gmail.com>
2018-03-23 20:19:05 +0200

    Order UI part

--- store-front/src/app/app.routing.ts
+++ store-front/src/app/app.routing.ts
@@ -1,9 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { MyAccountComponent } from './components/my-account/my-account.component';
-import { MyProfileComponent} from './components/my-profile/my-profile.component';
-import { BookListComponent} from './components/book-list/book-list.component';
+
+import {HomeComponent} from './components/home/home.component';
+import {MyAccountComponent} from './components/my-account/my-account.component';
+import {MyProfileComponent} from './components/my-profile/my-profile.component';
+import {BookListComponent} from './components/book-list/book-list.component';
+import {BookDetailComponent} from './components/book-detail/book-detail.component';
+import {ShoppingCartComponent} from './components/shopping-cart/shopping-cart.component';
+import {OrderComponent} from './components/order/order.component';
+import {OrderSummaryComponent} from './components/order-summary/order-summary.component';
+
 
 const appRoutes: Routes = [
   {
@@ -28,7 +34,23 @@ const appRoutes: Routes = [
   {
     path: 'bookList',
     component: BookListComponent
-  }
+  },
+  {
+		path: 'bookDetail/:id',
+		component: BookDetailComponent
+	},
+	{
+		path: 'shoppingCart',
+		component: ShoppingCartComponent
+	},
+	{
+		path: 'checkout',
+		component: OrderComponent
+	},
+	{
+		path: 'orderSummary',
+		component: OrderSummaryComponent
+	}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
